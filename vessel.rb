1336be2911af9d1668c875faf0d79f569c984d18
Devine Lu Linvega <aliceffekt@gmail.com>
2016-09-16 13:13:24 -0700

    Migrated to @actor/@target

--- vessel.rb
+++ vessel.rb
@@ -10,68 +10,75 @@ require_relative "object/recipe.rb"
 
 class Grimg
 
-	include Vessel
+  include Vessel
 
-	def http q = "Home"
+  class Actions
 
-		@searchRaw = q.to_s.gsub("-"," ")
-		@search = @searchRaw
-		@module = @searchRaw
+    include ActionCollection
 
-		if @searchRaw.include? ':'
-			@search = @searchRaw.split(':')[0]
-			@module = @searchRaw.split(':')[1]
-		end
+    def http q = "Home"
 
-		data = {
-			"search"  => @search,
-			"ingredients" => En.new("grim.ingredients").to_h,
-			"recipes" => En.new("grim.recipes").to_h,
-			"custom" => En.new("grim.custom").to_h
-		}
+      @searchRaw = q.to_s.gsub("-"," ")
+      @search = @searchRaw
+      @module = @searchRaw
 
-		$page = Page.new(data)
-		layout = Layouts.new(@search,@module)
+      if @searchRaw.include? ':'
+        @search = @searchRaw.split(':')[0]
+        @module = @searchRaw.split(':')[1]
+      end
 
-		return "
-		<!DOCTYPE html>
-		<html> 
-			<head>
-			<meta charset='UTF-8'>
-			<meta property='og:GrimGrains' content='grimgrains.com' />
-			<meta name='description' content='Grim Grains is a vegan food blog featuring over a hundred colourful, healthy, nut-free, and experimental recipes.'/>
+      data = {
+        "search"  => @search,
+        "ingredients" => En.new("grim.ingredients").to_h,
+        "recipes" => En.new("grim.recipes").to_h,
+        "custom" => En.new("grim.custom").to_h
+      }
 
-			<link rel='apple-touch-icon-precomposed' 	 href='../../img/interface/phone_xxiivv.png'/>
-			<link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
-			<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js' ></script>
-			<link rel='shortcut icon' href='http://grimgrains.com/favicon.ico' />
-			<link href='http://fonts.googleapis.com/css?family=Amatic+SC' rel='stylesheet' type='text/css'>
+      $page = Page.new(data)
+      layout = Layouts.new(@search,@module)
 
-			<link rel='stylesheet'           type='text/css'                 href='inc/style.reset.css?v=1' />
-			<link rel='stylesheet'           type='text/css'                 href='inc/style.main.css?v=1' />
-			<script 														 src='inc/jquery.main.js?v=1'></script>
+      return "
+<!DOCTYPE html>
+<html> 
+  <head>
+  <meta charset='UTF-8'>
+  <meta property='og:GrimGrains' content='grimgrains.com' />
+  <meta name='description' content='Grim Grains is a vegan food blog featuring over a hundred colourful, healthy, nut-free, and experimental recipes.'/>
 
-			<title>Grimgrains | #{$page.title}</title>
+  <link rel='apple-touch-icon-precomposed'   href='../../img/interface/phone_xxiivv.png'/>
+  <link href='http://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
+  <script src='https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js' ></script>
+  <link rel='shortcut icon' href='http://grimgrains.com/favicon.ico' />
+  <link href='http://fonts.googleapis.com/css?family=Amatic+SC' rel='stylesheet' type='text/css'>
 
-			<script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  <link rel='stylesheet'           type='text/css'                 href='inc/style.reset.css?v=1' />
+  <link rel='stylesheet'           type='text/css'                 href='inc/style.main.css?v=1' />
+  <script                              src='inc/jquery.main.js?v=1'></script>
 
-			ga('create', 'UA-53987113-1', 'auto');
-			ga('send', 'pageview');
-			</script>
+  <title>Grimgrains | #{$page.title}</title>
 
-		</head>
-			<body>"+layout.view+"</body>
-			"+layout.googleAnalytics+"
-			"+layout.sharePinterestSDK+"
-			"+layout.shareFacebookSDK+"
-			"+layout.googleAdsSDK+"
-			</html>
-			"
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		end
+  ga('create', 'UA-53987113-1', 'auto');
+  ga('send', 'pageview');
+  </script>
 
-	end
+</head>
+<body>"+layout.view+"</body>
+"+layout.googleAnalytics+"
+"+layout.sharePinterestSDK+"
+"+layout.shareFacebookSDK+"
+"+layout.googleAdsSDK+"
+</html>"
+
+    end
+
+  end
+
+  def actions ; return Actions.new(self,self) end
+
+end
